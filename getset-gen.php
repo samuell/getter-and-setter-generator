0c3bb7ac6823ce71b8dba2aab9ed9ca353a56d4e
Samuel Lampa <samuel.lampa@rilnet.com>
2011-11-04 18:22:12 +0100

    Init code variable + Change target of form, after file name change

--- getset-gen.php
+++ getset-gen.php
@@ -10,6 +10,9 @@
 <pre style="border: 1px solid #ccc; background: #efefef; 
             padding: .4em .7em; width: 566px;">
 <?php
+
+$code = '';
+
 if ( array_key_exists( 'code', $_POST )) {
 	$code = $_POST['code'];
 	preg_match_all( '/\$m[a-zA-Z0-9]+/', $code, $matchgroups );
@@ -19,7 +22,6 @@ if ( array_key_exists( 'code', $_POST )) {
 		$fieldName = lcfirst(substr( $memberFieldName, 1 ));
 		if ( count( $memberFieldName > 1 )) {
 		$fieldNameCapitalized = ucfirst($fieldName);
-			
 		# Generate the getters and setters
 		$getter	= "public function get$fieldNameCapitalized() { 
     return \$this->$memberFieldName;
@@ -38,8 +40,8 @@ public function set$fieldNameCapitalized( \$$fieldName ) {
 </pre>
 <?php } ?>
 
-<form action="getset.php" method="post">
-<textarea rows="7" cols="80" name="code"><?php echo($code) ?></textarea>
+<form action="getset-gen.php" method="post">
+<textarea rows="7" cols="80" name="code"><?php echo( $code ) ?></textarea>
 <br>
 <input type="submit" value="Generate Getters and Setters">
 
